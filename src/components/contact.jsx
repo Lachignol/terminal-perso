c0254852ebc0a6c4da56f7e7b46cfff0740f48f7
Lachignol <110435478+Lachignol@users.noreply.github.com>
2024-05-27 18:50:26 +0200

    mis en place du mail et de lien d'envoi

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,32 +1,47 @@
-import React from 'react';
+import React from "react";
 
 const Contacts = () => {
-const contacts=[
+  const contacts = [
     {
-        platform: "Github",
-        lien: "https://github.com/Lachignol",
-      },
+      platform: "Github",
+      lien: "https://github.com/Lachignol",
+    },
 
-      {
-        platform: "Linkedin",
-        lien: "https://linkedin.com/in/alexandre-scordilis",
-      },
-]
+    {
+      platform: "Linkedin",
+      lien: "https://linkedin.com/in/alexandre-scordilis",
+    },
+    {
+      platform: "Mail",
+      lien: "scordilisalexandre7@gmail.com",
+    },
+  ];
 
-
-    return (
-        <>
-         
-        <ul>
+  return (
+    <>
+      <ul>
         {contacts.map((contact, index) => (
-            <div key={index}>
-              <li>{contact.platform}</li>
-              <li><a href={contact.lien} target="_blank">Voici le lien</a></li>
-            </div>
-          ))}
-          </ul>
-          </>
-    );
+          <div key={index}>
+            {contact.platform == "Mail" ? (
+              <>
+                <li>{contact.platform}</li>
+                <a href={`mailto:${contact.lien}`}>{contact.lien}</a>
+              </>
+            ) : (
+              <>
+                <li>{contact.platform}</li>
+                <li>
+                  <a href={contact.lien} target="_blank">
+                    Voici le lien
+                  </a>
+                </li>
+              </>
+            )}
+          </div>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default Contacts;
