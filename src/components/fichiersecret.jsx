117f628ec62fd837176ef04b69243ec733851ad0
Lachignol <110435478+Lachignol@users.noreply.github.com>
2024-05-28 16:43:01 +0200

    ajoutfichiercaché

--- src/components/fichiersecret.jsx
+++ src/components/fichiersecret.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import photo from "../assets/image-fichier-caché.png"
 
 const Fichiersecret = () => {
 
   return (
     <div>
-      <h4 className="secret-titre">--Tu a trouvé le fichier caché !--</h4>
-      <p className="secret-content">contenu secret</p>
+      <h4 className="secret-titre">--Fichier caché !--</h4>
+      <img className='image-fichier-caché' src={photo}/>
+      <p className="secret-content">Felicitation!</p>
     </div>
   );
 };
