ed6da71b0f7ac493d2d129c5d1d692da12b5e74f
Lachignol <110435478+Lachignol@users.noreply.github.com>
2024-05-28 12:40:07 +0200

    titrefichiercaché

--- src/components/fichiersecret.jsx
+++ src/components/fichiersecret.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
 const Fichiersecret = () => {
+
   return (
     <div>
-      <h4 className="secret-titre">--Bien joué tu a trouvé le fichier caché--</h4>
+      <h4 className="secret-titre">--Tu a trouvé le fichier caché !--</h4>
       <p className="secret-content">contenu secret</p>
     </div>
   );
