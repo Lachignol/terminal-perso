0eec20f15765932ff08bf122d9d4ed85a807d0e6
La chignol <ton.email@example.com>
2026-01-13 21:22:30 +0100

    update-info

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,47 +1,47 @@
 import React from "react";
 
 const Contacts = () => {
-  const contacts = [
-    {
-      platform: "Github",
-      lien: "https://github.com/Lachignol",
-    },
+	const contacts = [
+		{
+			platform: "Github",
+			lien: "https://github.com/Lachignol",
+		},
 
-    {
-      platform: "Linkedin",
-      lien: "https://linkedin.com/in/alexandre-scordilis",
-    },
-    {
-      platform: "Mail",
-      lien: "scordilisalexandre7@gmail.com",
-    },
-  ];
+		{
+			platform: "Linkedin",
+			lien: "https://linkedin.com/in/alexandre-scordilis",
+		},
+		{
+			platform: "Mail",
+			lien: "ascordil@proton.me",
+		},
+	];
 
-  return (
-    <>
-      <ul>
-        {contacts.map((contact, index) => (
-          <div key={index}>
-            {contact.platform == "Mail" ? (
-              <>
-                <h4 className="contact-titre">--{contact.platform}--</h4>
-                <a  className="contact-lien" href={`mailto:${contact.lien}`}>{contact.lien}</a>
-              </>
-            ) : (
-              <>
-                <h4 className="contact-titre">--{contact.platform}--</h4>
-                <li>
-                  <a className="contact-lien" href={contact.lien} target="_blank">
-                    Voici le lien
-                  </a>
-                </li>
-              </>
-            )}
-          </div>
-        ))}
-      </ul>
-    </>
-  );
+	return (
+		<>
+			<ul>
+				{contacts.map((contact, index) => (
+					<div key={index}>
+						{contact.platform == "Mail" ? (
+							<>
+								<h4 className="contact-titre">--{contact.platform}--</h4>
+								<a className="contact-lien" href={`mailto:${contact.lien}`}>{contact.lien}</a>
+							</>
+						) : (
+							<>
+								<h4 className="contact-titre">--{contact.platform}--</h4>
+								<li>
+									<a className="contact-lien" href={contact.lien} target="_blank">
+										Voici le lien
+									</a>
+								</li>
+							</>
+						)}
+					</div>
+				))}
+			</ul>
+		</>
+	);
 };
 
 export default Contacts;
--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,85 +1,101 @@
 import React from "react";
 
 const Projects = () => {
-  const projects = [
-    {
-      titre: "Chat de gouttière",
-      description:
-        "Création d’un site d’administration/création de course de vélo urbaine.",
-      stack: "Node.Js-Express-Mongodb-React-Tailwind.",
-      lienGit: "https://github.com/Lachignol/chat_de_gouttiere",
-      lienActif: "",
-    },
-    {
-      titre: "Pari d'ami",
-      description: "Création d’une application mobile pour parier entre amis.",
-      stack: "Golang-PostgreSQL-React-Native.",
-      lienGit: "https://github.com/Lachignol/projet-mobile-pari_damis",
-      lienActif: "",
-    },
-    {
-      titre: "Microblogging",
-      description: "Création d’un blog.",
-      stack: "Laravel(php)-PostgreSQL-Tailwinds.",
-      lienGit:
-        "https://github.com/Lachignol/projet-collectif-microblogging-alexandre-jeremie-claire",
-      lienActif: "",
-    },
-    {
-      titre: "Noyales",
-      description: "Création d'un site E-commerce dédié à la vente de meubles.",
-      stack: "Node.js-Express-MangoDb-React.",
-      lienGit: "https://github.com/Lachignol/E-commerce-vente-de-meuble",
-      lienActif: "",
-    },
-    {
-      titre: "Voisinous",
-      description: "Création d’un reseau social entre voisins.",
-      stack: "Php-Mysql-Apache.",
-      lienGit: "https://github.com/Lachignol/Reseau-social-php",
-      lienActif: "",
-    },
-    {
-      titre: "Jouvence",
-      description:
-        "Extension permetant de traduire une page web en langage de 'jeunes'.",
-      stack: "Html-Css-Javascript.",
-      lienGit: "https://github.com/Lachignol/Extension-navigateur",
-      lienActif: "",
-    },
-    {
-      titre: "Quality air map",
-      description:
-        "Indices de pollution selon la localisation sur une carte du monde.",
-      stack: "Html-Css-Javascript.",
-      lienGit: "https://github.com/Lachignol/Dataviz",
-      lienActif: "",
-    },
-  ];
+	const projects = [
+		{
+			titre: "chat_lachignol",
+			description:
+				"Messagerie en temps réel sur TCP chiffrée via SSL/TLS.",
+			stack: "C.",
+			lienGit: "https://github.com/Lachignol/chat_lachignol",
+			lienActif: "",
+		},
+		{
+			titre: "Chat de gouttière",
+			description:
+				"Création d’un site d’administration/création de course de vélo urbaine.",
+			stack: "Node.Js-Express-Mongodb-React-Tailwind.",
+			lienGit: "https://github.com/Lachignol/chat_de_gouttiere",
+			lienActif: "",
+		},
+		{
+			titre: "Pari d'ami",
+			description: "Création d’une application mobile pour parier entre amis.",
+			stack: "Golang-PostgreSQL-React-Native.",
+			lienGit: "https://github.com/Lachignol/projet-mobile-pari_damis",
+			lienActif: "",
+		},
+		{
+			titre: "lachignol Blog",
+			description:
+				"blog en Go avec rendu de Markdown en HTML.",
+			stack: "Golang.",
+			lienGit: "https://github.com/Lachignol/lachignol--blog",
+			lienActif: "https://blog.lachignol.com",
+		},
+		{
+			titre: "Martin solving",
+			description:
+				"Application-cli multi-outils.",
+			stack: "Golang-Cobra.",
+			lienGit: "https://github.com/Lachignol/martin-solving",
+			lienActif: "",
+		},
+		{
+			titre: "Microblogging",
+			description: "Création d’un blog.",
+			stack: "Laravel(php)-PostgreSQL-Tailwinds.",
+			lienGit:
+				"https://github.com/Lachignol/projet-collectif-microblogging-alexandre-jeremie-claire",
+			lienActif: "",
+		},
+		{
+			titre: "Noyales",
+			description: "Création d'un site E-commerce dédié à la vente de meubles.",
+			stack: "Node.js-Express-MangoDb-React.",
+			lienGit: "https://github.com/Lachignol/E-commerce-vente-de-meuble",
+			lienActif: "",
+		},
+		{
+			titre: "Voisinous",
+			description: "Création d’un reseau social entre voisins.",
+			stack: "Php-Mysql-Apache.",
+			lienGit: "https://github.com/Lachignol/Reseau-social-php",
+			lienActif: "",
+		},
+		{
+			titre: "Jouvence",
+			description:
+				"Extension permetant de traduire une page web en langage de 'jeunes'.",
+			stack: "Html-Css-Javascript.",
+			lienGit: "https://github.com/Lachignol/Extension-navigateur",
+			lienActif: "",
+		},
+	];
 
-  return (
-    <>
-      <ul>
-        {projects.map((project, index) => (
-          <div key={index}>
-            <h4 className="projet-titre">--{project.titre}--</h4>
-            <li className="projet-description">{project.description}</li>
-            <li className="projet-stack">Stack:{project.stack}</li>
-            <li>
-              <a href={project.lienGit} target="_blank">
-                lien github
-              </a>
-            </li>
-            {project.lienActif && (
-              <li>
-                <a href={project.lienActif}>lien du site</a>
-              </li>
-            )}
-          </div>
-        ))}
-      </ul>
-    </>
-  );
+	return (
+		<>
+			<ul>
+				{projects.map((project, index) => (
+					<div key={index}>
+						<h4 className="projet-titre">--{project.titre}--</h4>
+						<li className="projet-description">{project.description}</li>
+						<li className="projet-stack">Stack:{project.stack}</li>
+						<li>
+							<a href={project.lienGit} target="_blank">
+								lien github
+							</a>
+						</li>
+						{project.lienActif && (
+							<li>
+								<a href={project.lienActif}>lien du site</a>
+							</li>
+						)}
+					</div>
+				))}
+			</ul>
+		</>
+	);
 };
 
 export default Projects;
